2e00567592986de57c3642d15053a2b92fcb23d2
Fabian Canas <fcanas@gmail.com>
2014-09-14 22:17:43 -0400

    Empty category

---
+++ Pod/Classes/RLMObject+Beanstalk.h
@@ -0,0 +1,13 @@
+//
+//  RLMObject+Beanstalk.h
+//  Beanstalk
+//
+//  Created by Fabian Canas on 09/14/2014.
+//  Copyright (c) 2014 Fabian Canas. All rights reserved.
+//
+
+#import "RLMObject.h"
+
+@interface RLMObject (Beanstalk)
+
+@end
